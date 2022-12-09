1addd16e8c1deb1f5ea606550e796aac1599c806
sten <mangastens@gmail.com>
2022-12-09 21:09:34 +0100

    update url

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,9 +6,10 @@ import Head from 'next/head';
 export default function Home() {
   const [data, setData] = useState<ApiElement[]>();
   async function getUrls() {
-    const res = await axios.get("/urls");
+    const res = await axios.get(process.env.NEXT_PUBLIC_API_ADRESS + '/urls');
     const apiData: ApiElement[] = res.data;
     setData(apiData);
+
   };
   useEffect(() => {
     getUrls();
