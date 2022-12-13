cd3a5617aee7000c6c99702349cb1062b4ca9731
sten <mangastens@gmail.com>
2022-12-13 20:42:54 +0100

    api

---
+++ backend/__tests__/endpoint.test.js
@@ -0,0 +1,19 @@
+//https://zellwk.com/blog/endpoint-testing/
+// const app = require('./index') // Link to your server file
+// const supertest = require('supertest')
+// const request = supertest(app)
+
+import app from "../src/app"
+import request from "supertest";
+
+describe("GET / - a simple api endpoint", () => {
+    it("Hello API Request", async () => {
+        const res = await request(app)
+            .get("/urls")
+        console.log(typeof res.body[0].url);
+        expect(res.statusCode).toEqual(200);
+        expect(typeof res.body[0].url).toEqual("string")
+    });
+});
+
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,50 +1,7 @@
 /* eslint-disable max-len */
 import app from './app';
-import { ApiElement } from '../types/type';
-import { getUrls, urlSearch, testSpeed } from './controller';
 
-app.post(`/speedurl`, async (req, res) => {
-  //spawns playwright to get speed of url. 
-  try {
-    type req_body = {
-      url: string,
-      match: string
-    }
-    const req_bod = req.body as req_body;
-    const url: string = req_bod.url;
-    const match: string = req_bod.match;
-    const speedReturn: ApiElement[] = await testSpeed(url, match);
-    // reverse to get chronolgical order 
-    res.json(speedReturn.reverse());
-  } catch (error) {
-    console.log("error fetching speed", error);
-    res.status(400);
-  }
-});
 
-app.get('/urls', async (req, res) => {
-  //fetch array from db.json
-  try {
-    res.json(await getUrls());
-  } catch (error) {
-    console.log("cant get data from db", error);
-    res.status(400);
-  }
-});
-app.get('/search', async (req, res) => {
-  //just filter an array for including text on each element
-  try {
-    if (req.query?.url != undefined) {
-      const reqQuery = req.query?.url.toString();
-      res.json(await urlSearch(reqQuery));
-    }
-  } catch (error) {
-    res.status(400);
-  }
-});
 app.listen(process.env.PORT || 1339, () => {
   console.log(`Example app listening on port ${process.env.PORT || 1339}`);
 });
-
-// const { firefox } = require('playwright');  // Or 'chromium' or 'webkit'.
-
